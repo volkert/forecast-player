08bf819a50bab32773710bae8b531abb0a6741d9
Volker Tietz <volker.tietz@xing.com>
2015-07-28 22:42:52 +0200

    Working waveform with time indicator

--- web/app/waveform_directive.js
+++ web/app/waveform_directive.js
@@ -6,16 +6,20 @@ angular.module('soundcloud-player')
       link:     function (scope, elm, attr, playerCtrl) {
         var gradient,
             waveform,
-            width = 380;
+            ctx,
+            canvas,
+            height = 40,
+            width  = 380;
 
         waveform = new Waveform({
           container:   elm[0],
-          height:      40,
+          height:      height,
           width:       width,
           interpolate: true
         });
 
-        gradient = waveform.context.createLinearGradient(0, 0, 0, waveform.height);
+        ctx = waveform.context;
+        gradient = ctx.createLinearGradient(0, 0, 0, waveform.height);
         gradient.addColorStop(0.0, "#ECE9E6");
         gradient.addColorStop(1.0, "#fff");
         waveform.innerColor = gradient;
@@ -24,6 +28,7 @@ angular.module('soundcloud-player')
         function repaint(track) {
           if (track) {
             waveform.dataFromSoundCloudTrack(track);
+
             elm.find('canvas')[0].addEventListener('click', function (event) {
               var percent = Math.floor(event.layerX / width * 100) / 100;
               var newTime = Math.floor(scope.Playlist.currentTrack.duration * percent) / 60000;
@@ -32,6 +37,25 @@ angular.module('soundcloud-player')
             });
           }
         }
+
+        scope.$watch('timeElapsed', function (timeElapsed) {
+          if (timeElapsed && ctx) {
+            var timeTotal = scope.Playlist.currentTrack.duration / 1000;
+            var percent = timeElapsed / (timeTotal / 100) * 100;
+            percent = Math.round(percent) / 10000;
+
+            waveform.update({ data: waveform.data });
+
+            ctx.globalAlpha = 0.3;
+            gradient = ctx.createLinearGradient(0, 0, waveform.height, width * percent);
+            gradient.addColorStop(0.0, "#fe8c00");
+            gradient.addColorStop(1.0, "#f83600");
+
+            ctx.fillStyle = gradient;
+            ctx.fillRect(0, 0, width * percent, waveform.height);
+            ctx.globalAlpha = 1.0;
+          }
+        });
       }
     }
   }
